1e89a3fd6104d1329f29a92cb8f007915321b5c2
andressadiniz <84856154+Andressa172@users.noreply.github.com>
2024-05-21 20:05:20 -0300

    incompleto

---
+++ TadDeque.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<conio.h>
+
+struct TpPont{
+	char Elem;
+	TpPon *ant, *prox;
+};
+
+struct TpDEQUE{
+	TpPont *Cab, *Cauda;
+};
+
+void Inicializar(TpDEQUE &Desc){
+	Desc.Cab = Desc.Cauda = NULL;
+}
+
+int Vazio(){
+	
+	if(){
+		
+	}
+}
+
+TpPont *InserirCabeca(){
+	
+}
+
+TpPont *InserirCauda(){
+	
+}
+
+TpPont *RetirarCabeca(){
+	
+}
+
+TpPont *RetirarCauda(){
+	
+}
