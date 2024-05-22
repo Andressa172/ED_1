eeb572b3cf1b9265875854f0b6f5b0159cb6cb65
andressadiniz <84856154+Andressa172@users.noreply.github.com>
2024-05-22 18:20:32 -0300

    Add files via upload

--- TadDeque.cpp
+++ TadDeque.cpp
@@ -5,36 +5,109 @@
 
 struct TpPont{
 	char Elem;
-	TpPon *ant, *prox;
+	TpPont *ant, *prox;
 };
 
 struct TpDEQUE{
 	TpPont *Cab, *Cauda;
 };
 
-void Inicializar(TpDEQUE &Desc){
-	Desc.Cab = Desc.Cauda = NULL;
+void Inicializar(TpDEQUE *Desc){
+	Desc->Cab =NULL;
+	Desc->Cauda = NULL;
 }
 
-int Vazio(){
+int Vazio(TpDEQUE *Desc){
 	
-	if(){
-		
+	return (Desc->Cab==NULL);
+}
+
+TpPont *InserirCabeca(TpDEQUE *Desc, char Elem){
+	TpPont *novo = new TpPont;
+	novo->Elem = Elem;
+	novo->ant = NULL;
+	novo->prox = Desc->Cab;
+	return novo;
+}
+
+TpPont *InserirCauda(TpDEQUE *Desc, char Elem){
+	TpPont *novo = new TpPont;
+	novo->Elem=Elem;
+	novo->ant = Desc->Cauda;
+	novo->prox = NULL;
+}
+
+TpPont *RetirarCabeca(TpDEQUE *Desc){
+	if(Desc->Cab==NULL){
+		return NULL;
 	}
+	
+	TpPont *remove = Desc->Cab;
+	Desc->Cab = Desc->Cab->prox;
+
+    if (Desc->Cab == NULL) {
+        Desc->Cauda = NULL;
+    } else {
+        Desc->Cab->ant = NULL;
+    }
+
+    remove->prox = NULL;
+    return remove;
 }
 
-TpPont *InserirCabeca(){
-	
+TpPont *RetirarCauda(TpDEQUE *Desc) {
+    if (Desc->Cauda == NULL) {
+        return NULL;
+    }
+
+    TpPont *remove = Desc->Cauda;
+    Desc->Cauda = Desc->Cauda->ant;
+
+    if (Desc->Cauda == NULL) {
+        Desc->Cab = NULL;
+    } else
+        Desc->Cauda->prox = NULL;
+    
+
+    remove->ant = NULL;
+    return remove;
 }
 
-TpPont *InserirCauda(){
-	
+
+
+/*
+void ImprimirDEQUE(TpDEQUE *Desc) {
+    TpPont *atual = Desc->Cab;
+    while (atual != NULL) {
+        printf("%c ", atual->Elem);
+        atual = atual->prox;
+    }
+    printf("\n");
 }
 
-TpPont *RetirarCabeca(){
+int main() {
+    TpDEQUE deque;
+    Inicializar(&deque);
 	
-}
+    InserirCabeca(&deque, 'A');
+    InserirCabeca(&deque, 'B');
+    InserirCauda(&deque, 'C');
+    InserirCauda(&deque, 'D');
 
-TpPont *RetirarCauda(){
-	
+    ImprimirDEQUE(&deque);
+
+    TpPont *remove = RetirarCabeca(&deque);
+    if (remove != NULL) {
+        printf("Removido da cabeca: %c\n", remove->Elem);
+        delete remove;
+    }
+
+    remove = RetirarCauda(&deque);
+    if (remove != NULL) {
+        printf("Removido da cauda: %c\n", remove->Elem);
+        delete remove;
+    }
+    ImprimirDEQUE(&deque);
+    return 0;
 }
+*/
